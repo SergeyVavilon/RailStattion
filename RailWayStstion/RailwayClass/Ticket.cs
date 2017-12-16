738bd74fc1bcb31198676c1629b7fda9fca2bee9
Максим Курганский <kurgansky.maxim@yandex.ru>
2017-12-16 22:21:12 +0300

    classes
    add

---
+++ RailWayStstion/RailwayClass/Ticket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayClass
+{
+    class Ticket
+    {
+        private int id;
+
+        public int TicketID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string route;
+
+        public string Route
+        {
+            get { return route; }
+            set { route = value; }
+        }
+
+
+    }
+}
---
+++ RailWayStstion/RailwayClass/Train.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RailwayClass
+{
+    public class Train
+    {
+        private int train_id;
+
+        public int Train_ID
+        {
+            get { return train_id; }
+            set { train_id = value; }
+        }
+
+        private string depart_station;
+
+        public string StationofDeparture
+        {
+            get { return depart_station; }
+            set { depart_station = value; }
+        }
+
+        private string arrival_station;
+
+        public string StationofArrival
+        {
+            get { return arrival_station; }
+            set { arrival_station = value; }
+        }
+
+        private int cost;
+
+        public int TravelCost
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+
+        public Train(int id, string depart, string arrival, int price)
+        {
+            Train_ID = id;
+            StationofDeparture = depart;
+            StationofArrival = arrival;
+            TravelCost = price;
+        }
+
+        public string TrainRoute()
+        {
+            return StationofDeparture + "-" + StationofArrival;
+        }
+    }
+}
