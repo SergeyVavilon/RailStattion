9a0e4c463072d5e2f7c00572ca7a4b642719569c
Максим Курганский <kurgansky.maxim@yandex.ru>
2017-12-16 22:47:51 +0300

    train add

---
+++ RailWayStstion/design/Train.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design
+{
+    class Train
+    {
+        private int train_id;
+
+        public int Train_ID
+        {
+            get { return train_id; }
+            set { train_id = value; }
+        }
+
+    }
+}
